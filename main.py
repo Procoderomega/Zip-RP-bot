e237ea049f0ebe400f2cf5e04bd06a4858d6a17c
Procoderomega <gomballestas@gmail.com>
2026-03-13 14:01:40 -0600

    v1.0.0
    - `.gitignore` file
    - `changelog.md` file
    - `main.py` file

---
+++ main.py
@@ -0,0 +1,24 @@
+import discord
+from dotenv import load_dotenv
+import os
+from discord.ext import commands
+
+#* Loading .env variables
+load_dotenv()
+TOKEN = os.getenv("TOKEN")
+
+#* Configuring intents
+intents = discord.Intents.default()
+intents.message_content = True
+
+#* Declaring MyClient
+MyClient = commands.Bot(command_prefix="^",intents=intents)
+
+#* On ready event
+@MyClient.event
+async def on_ready():
+    print(f"{MyClient.user} Serving 🛜...\nZip ID 🪪: {MyClient.user.id}")
+print(f"token: {TOKEN}")
+#* Initializing bot
+if __name__ == "__main__":
+    MyClient.run(TOKEN)
